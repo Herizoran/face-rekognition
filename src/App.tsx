ac1e0fdf3bccf07084b8c1336e80920ae05bbdc2
herizoran <herizo.ran@gmail.com>
2022-08-09 13:50:56 +0300

    added browse file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
+import './components/formStyle.css'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="m-5 flex justify-center">
+            <form className="flex items-center space-x-6">
+                <label className="block">
+                    <h1 className="sr-only">Choose profile photo</h1>
+                    <br/>
+                    <input type={"file"}
+                            className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-amber-400 file:text-black hover:file:bg-violet-100"/>
+                </label>
+            </form>
+        </div>
+    );
 }
 
 export default App;
